ef9f64afb285d553b9633cdc8904979c4b237b24
Joseph Havlik <joeh@occinc.com>
2019-05-31 16:46:38 -0500

    Added in tkinter if dial called without args

--- dial.py
+++ dial.py
@@ -3,27 +3,37 @@ import sys
 import configparser
 import urllib.request
 import os
+import tkinter
+
+
+def filter_num(number):
+    valid_nums = ['0', '1', '2', '3', '4', '5', '6', '7', 
+                  '8', '9', '*', '#']
+    if number.upper() == 'VM':
+            return ['VM']
+    clean_num = []
+    for l in list(number):
+        if l not in valid_nums:
+            print('Invalid digit')
+            sys.exit(1)
+        else:
+            if l == '#':
+                clean_num.append('HASH')
+            elif l == '*':
+                clean_num.append('STAR')
+            else:
+                clean_num.append(l)
+    return clean_num
 
 
 def parse_phone(number, send):
-    if len(number) > 12:
-        print('Phone number has to many digits please double check number.')
-        sys.exit(1)
-    else:
-        prep_number = list(number)
-        if send:
-            prep_number.append('SEND')
-        return ':'.join(prep_number)
+    clean_num = filter_num(number)
+    if send:
+        clean_num.append('SEND')
+    return ':'.join(clean_num)
 
 
-def main():
-    """
-    This takes care of sending digits to your grandstream phone and optionally dialing.
-    """
-    if len(sys.argv) < 2:
-        print('Please enter the digits dialed as a command line argument')
-        sys.exit(1)
-
+def get_config():
     config = configparser.ConfigParser()
     conf_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'dial_config.ini')
@@ -36,9 +46,15 @@ def main():
 
     dial = config['dial']
     if not dial:
-        print('Improperly prepared config file dial section is not found, please check spelling')
+        print('Improperly prepared config file dial section is not found, \
+               please check spelling')
         sys.exit(1)
+    else:
+        return dial
 
+
+def send_keys(keys):
+    dial = get_config()
     host = dial.get('host') or 'localhost'
     passcode = dial.get('passcode') or 'admin'
     send = dial.get('send') or 'true'
@@ -53,21 +69,49 @@ def main():
                   'Upgrade-Insecure-Requests': 1,
                   'Pragma': 'no-cache',
                   'Cache-Control': 'no-cache', }
-
-    if sys.argv[1].isdigit():
-        num = parse_phone(sys.argv[1], add_send)
-    else:
-        print('Please ensure phone number entered contains only numbers')
-        sys.exit()
-
+    num = parse_phone(keys, add_send)
     req = urllib.request.Request('http://{}/cgi-bin/api-send_key?passcode={}&keys={}'.format(host, passcode, num),
                                  headers=req_header)
     try:
         with urllib.request.urlopen(req) as response:
             if response.status != 200:
-                print('Request returned a {}:{} please check your configuration and try again.'.format(response.status, response.reason))
+                print('Request returned a {}:{} please check your \
+                       configuration and try again.'.format(response.status,
+                                                            response.reason))
     except urllib.error.URLError:
         print('There was an error with the url that accessed.  Attempted to access {}'.format(req))
+
+
+def main():
+    """
+    This takes care of sending digits to your grandstream
+    phone and optionally dialing.
+    """
+    if len(sys.argv) < 2:
+        def get_text():
+            send_keys(e1.get())
+        master = tkinter.Tk()
+        master.title("GS Dial")
+        tkinter.Label(master, text="Enter Phone Number").grid(row=0)
+        e1 = tkinter.Entry(master)
+        e1.grid(row=0, column=1)
+        tkinter.Button(master,
+                       text='Quit',
+                       command=master.quit).grid(row=3,
+                                                 column=0,
+                                                 sticky=tkinter.W,
+                                                 pady=4)
+        tkinter.Button(master,
+                       text='Dial',
+                       command=get_text).grid(row=3,
+                                              column=1,
+                                              sticky=tkinter.W,
+                                              pady=4)
+        e1.bind('<Return>', lambda _: get_text())
+        e1.bind('<KP_Enter>', lambda _: get_text())
+        tkinter.mainloop()
+    else:
+        send_keys(sys.argv[1])
 
 if __name__ == "__main__":
     main()
